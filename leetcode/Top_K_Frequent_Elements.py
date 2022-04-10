b34c78f308e52de48e91e604c3252d7703a0ae4e
shubham8patni <shubhampatni88@gmail.com>
2022-04-10 20:17:14 +0530

    April 10

--- leetcode/Top_K_Frequent_Elements.py
+++ leetcode/Top_K_Frequent_Elements.py
@@ -1,9 +1,8 @@
 class Solution:
     def topKFrequent(self, nums, k):
-        cnt = Counter(nums)
         if len(nums)==1:
             return nums
         sol = []
-        for i in cnt.most_common(k):
+        for i in Counter(nums).most_common(k):
             sol.append(i[0])
         return sol
