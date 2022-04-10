9a9cf15aa515509e8b803573f134d6a8bd0bf9b2
shubham8patni <shubhampatni88@gmail.com>
2022-04-10 19:56:56 +0530

    April 10

--- leetcode/Baseball_Game.py
+++ leetcode/Baseball_Game.py
@@ -12,3 +12,19 @@ class Solution:
                 x.append(int(i))
         total = sum(x)        
         return total  
+
+    
+    # # A little bit faster
+    def calPoints(self, ops) -> int:
+        x = []
+        for i in ops:
+            if i == "D":
+                x.append(2*x[-1])
+            elif i == "C":
+                x.pop()
+            elif i == "+":
+                x.append(x[-1]+x[-2])
+            else:
+                x.append(int(i))
+        total = sum(x)        
+        return total  
