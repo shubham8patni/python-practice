3f20e4afc6a455c4a727911d8cf0bd2b1d77051f
shubham8patni <shubhampatni88@gmail.com>
2022-05-01 03:00:45 +0530

    April 30

--- leetcode/Evaluate_Division.py
+++ leetcode/Evaluate_Division.py
@@ -1,9 +1,17 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    def calcEquation(self, equations, values, queries):
         graph = defaultdict(dict)
         N = len(equations)
         for i in range(N):
             graph[equations[i][0]][equations[i][1]] = values[i]
             graph[equations[i][1]][equations[i][0]] = 1/values[i]
             
-            print(graph)
+
+        def dfs(x,y,visited):
+            print(x,y,visited)
+
+
+        output = []
+        for p,q in queries:
+            output.append(dfs(p, q, set()))
+        
