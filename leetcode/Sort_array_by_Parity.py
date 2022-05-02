7c4d328ffdf6ade64ded509df7e5dbbe26300230
shubham8patni <shubhampatni88@gmail.com>
2022-05-03 01:08:30 +0530

    May 2

---
+++ leetcode/Sort_array_by_Parity.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+
+        left , right = 0, len(nums) - 1
+        while (left< right):
+            if nums[left]%2 !=0:
+                nums[left], nums[right] = nums[right], nums[left]
+                right-=1
+            else:
+                left+=1
+        return nums
