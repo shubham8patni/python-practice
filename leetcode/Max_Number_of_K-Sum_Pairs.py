ea465e57af2621ca7c702bc49d5b72fccd17c4ab
shubham8patni <shubhampatni88@gmail.com>
2022-05-05 02:14:45 +0530

    May 4

--- leetcode/Max_Number_of_K-Sum_Pairs.py
+++ leetcode/Max_Number_of_K-Sum_Pairs.py
@@ -1,7 +1,12 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         counter = defaultdict(int)
-            count = 0
-            for i in nums:
-                remain = k - i
-                print(remain, count,counter)
+        count = 0
+        for i in nums:
+            remain = k - i
+            if counter[remain]>0:
+                counter[remain]-=1
+                count+=1
+            else:
+                counter[i] +=1
+            print(remain, count,counter)
