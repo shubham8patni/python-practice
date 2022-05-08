d11657f15190ed4944892d86ae75b0f85eb23d58
shubham8patni <shubhampatni88@gmail.com>
2022-05-08 22:44:25 +0530

    May 7

--- leetcode/132_Pattern.py
+++ leetcode/132_Pattern.py
@@ -1,3 +1,24 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []        
+        for i in range(1,len(nums)):
+			
+            while stack and nums[i]>=stack[-1][0]:
+                stack.pop()
+            
+            if stack and nums[i] > stack[-1][1]:
+                return True 
+            
+            stack.append([nums[i],nums[0]])
+			
+			
+            nums[0] = min(nums[0],nums[i])
+            
+        return False
+
+
+
+
 # # O(n^3) causes time limit exceed
 
 
