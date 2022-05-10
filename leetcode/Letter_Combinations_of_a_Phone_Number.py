e13c22c424d31588df683fbf3d164045cced8f10
shubham8patni <shubhampatni88@gmail.com>
2022-05-10 22:52:56 +0530

    May 9

--- leetcode/Letter_Combinations_of_a_Phone_Number.py
+++ leetcode/Letter_Combinations_of_a_Phone_Number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+    def letterCombinations(self, digits: str):
         dictn = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
         res = []
         
