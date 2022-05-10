4e898e188b9778a339c1be99ad5f9681970df53a
shubham8patni <shubhampatni88@gmail.com>
2022-05-10 22:43:01 +0530

    May 9

---
+++ leetcode/Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,35 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dictn = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        res = []
+        
+        if digits == "":
+            return res
+        
+        
+        
+        if len(digits)==1:
+            res = dictn[digits]
+            return res
+        elif len(digits)==2:    
+            for i in dictn[digits[0]]:
+                for j in dictn[digits[1]]:
+                    temp = i+j
+                    res.append(str(temp))
+            return res
+        elif len(digits)==3:    
+            for i in dictn[digits[0]]:
+                for j in dictn[digits[1]]:
+                    for k in dictn[digits[2]]:
+                        temp = i+j+k
+                        res.append(str(temp))
+            return res
+        elif len(digits)==4:    
+            for i in dictn[digits[0]]:
+                for j in dictn[digits[1]]:
+                    for k in dictn[digits[2]]:
+                        for l in dictn[digits[3]]:
+                            temp = i+j+k+l
+                            res.append(str(temp))
+            return res
+                    
