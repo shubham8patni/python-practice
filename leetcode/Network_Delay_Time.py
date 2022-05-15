f68e6866aa0e753a2c2521efd93830d236924784
shubham8patni <shubhampatni88@gmail.com>
2022-05-15 23:23:17 +0530

    May 14

--- leetcode/Network_Delay_Time.py
+++ leetcode/Network_Delay_Time.py
@@ -5,4 +5,17 @@ class Solution:
         for x,y,w in times:
             adj_list[x].append((w, y))
 
-        print(adj_list)
+        visited=set()
+        heap = [(0, k)]
+        while heap:
+            travel_time, node = heapq.heappop(heap)
+            visited.add(node)
+            
+            if len(visited)==n:
+                return travel_time
+            
+            for time, adjacent_node in adj_list[node]:
+                if adjacent_node not in visited:
+                    heapq.heappush(heap, (travel_time+time, adjacent_node))
+                
+        return -1
