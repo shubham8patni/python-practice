348251b9860e547805f1b814c88eebe7cb720551
shubham8patni <shubhampatni88@gmail.com>
2022-05-17 21:30:53 +0530

    May 17

--- leetcode/Shortest_Path_in_Binary_Matrix.py
+++ leetcode/Shortest_Path_in_Binary_Matrix.py
@@ -8,4 +8,19 @@ class Solution:
         grid[0][0] = 1
         frontier = [(0, 0)] # start from top-left
         count = 1 # at least 1 length
-        
+        if n == 1: return count # edge case
+        while frontier:
+            adjs = [] # all adjacent cells
+            count += 1
+            for x, y in frontier:
+                adj = [] # adjacent cells of current cell
+                for ix, iy in moves:
+                    i = x + ix
+                    j = y + iy
+                    if 0 <= j < n and 0 <= i < n and grid[i][j] == 0:
+                        if i == j == n - 1: return count# reach the destination
+                        grid[i][j] = 1 # visited
+                        adj.append((i, j))
+                adjs.extend(adj)
+            frontier = adjs # switch to next layer
+        return -1
